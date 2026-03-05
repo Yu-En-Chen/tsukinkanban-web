11814468e28580611a7483eeb0657df6621352f7
ヨオン <eqq1029@gmail.com>
2026-03-05 21:44:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -849,14 +849,17 @@ function initDismissIcon() {
     if (document.getElementById('dismiss-icon')) return;
 
     const svgContent = `
-        <svg xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="100%" height="100%">
-            <path d="M18.71,8.21a1,1,0,0,0-1.42,0l-4.58,4.58a1,1,0,0,1-1.42,0L6.71,8.21a1,1,0,0,0-1.42,0,1,1,0,0,0,0,1.41l4.59,4.59a3,3,0,0,0,4.24,0l4.59-4.59A1,1,0,0,0,18.71,8.21Z"/>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="100%" height="100%">
+            <path d="M18 6 6 18"/>
+            <path d="m6 6 12 12"/>
         </svg>`;
 
     const iconDiv = document.createElement('div');
     iconDiv.id = 'dismiss-icon';
     iconDiv.className = 'dismiss-icon';
     iconDiv.innerHTML = svgContent;
+    // 確保新圖示吃的到線條顏色
+    iconDiv.style.color = 'rgba(142, 142, 147, 0.8)';
     
     document.body.appendChild(iconDiv);
 }
