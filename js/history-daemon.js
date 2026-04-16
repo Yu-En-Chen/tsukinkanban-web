a9d7a1baf5c97cf2e7ec7d38d722c391f03f547b
chen yuen <eqq1029@gmail.com>
2026-04-17 02:27:44 +0800

    加入?t=${Date.now()}

--- js/history-daemon.js
+++ js/history-daemon.js
@@ -82,7 +82,7 @@ async function fetchHistoryDaemon() {
                         finalId = `Departure_${id}`; 
                     }
 
-                    const url = `https://api.tsukinkanban.com/api/history/${type}/${finalId}`;
+                    const url = `https://api.tsukinkanban.com/api/history/${type}/${finalId}?t=${Date.now()}`;
                     let routeName = (window.MasterRouteDictionary && window.MasterRouteDictionary[id]) ? window.MasterRouteDictionary[id].name : id;
                     routeName = routeName.replace('Departure_', '出發 ').replace('Arrival_', '抵達 ');
 
