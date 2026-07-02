fb790b2b3ebe253fcb7f925f6c1f3e47d4058b28
chen yuen <eqq1029@gmail.com>
2026-07-02 18:54:35 +0800

    臨時飛機卡片svg取消

--- js/flights.js
+++ js/flights.js
@@ -506,13 +506,9 @@ window.previewFlightFromSearch = function(routeId) {
         defaultHex = '#87C643'; // Solaseed 九州綠
     }
 
-    const takeoffIconSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" style="vertical-align: text-bottom; margin-right: 4px;"><path d="M2 22h20"/><path d="M6.36 17.4 4 17l-2-4 1.1-.55a2 2 0 0 1 1.8 0l.17.1a2 2 0 0 0 1.8 0L8 12 5 6l.9-.45a2 2 0 0 1 2.09.2l4.02 3a2 2 0 0 0 2.1.2l4.19-2.06a2.41 2.41 0 0 1 1.73-.17L21 7a1.4 1.4 0 0 1 .87 1.99l-.38.76c-.23.46-.6.84-1.07 1.08L7.58 17.2a2 2 0 0 1-1.22.18Z"/></svg>`;
-    const landingIconSvg = `<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round" style="vertical-align: text-bottom; margin-right: 4px;"><path d="M2 22h20"/><path d="M3.77 10.77 2 9l2-4.5 1.1.55c.55.28.9.84.9 1.45s.35 1.17.9 1.45L8 8.5l3-6 1.05.53a2 2 0 0 1 1.09 1.52l.72 5.4a2 2 0 0 0 1.09 1.52l4.4 2.2c.42.22.78.55 1.01.96l.6 1.03c.49.88-.06 1.98-1.06 2.1l-1.18.15c-.47.06-.95-.02-1.37-.24L4.29 11.15a2 2 0 0 1-.52-.38Z"/></svg>`;
-    const flightTypeIcon = flight.type === 'Departure' ? takeoffIconSvg : landingIconSvg;
-
     const tempCard = {
-        id: 'temp-search-flight', 
-        name: `${flightTypeIcon}${fid}`, 
+        id: 'temp-search-flight',
+        name: fid,
         hex: defaultHex, 
         desc: formatted.desc, 
         message: formatted.message, // ✨ 會將 message 傳給 UI 去生成玻璃面板
